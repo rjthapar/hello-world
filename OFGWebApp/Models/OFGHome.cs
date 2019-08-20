efaf6a7af3c660bd607a859c31cc831744ea5665
rjthapar <41053985+rjthapar@users.noreply.github.com>
2019-08-20 05:36:28 -0400

    after-calendar-changes

--- OFGWebApp/Models/OFGHome.cs
+++ OFGWebApp/Models/OFGHome.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -12,14 +13,27 @@ namespace OFGWebApp.Models
         //model to contain all the values for Weekly JIRA Updates
         [System.ComponentModel.DataAnnotations.Key]
         public int Id { get; set; }
+        [Display(Name ="User Id")]
         public string userId { get; set; }
+        [Display(Name = "JIRA Number")]
         public int jiraNumber { get; set; }
+        [Display(Name = "Application Description")]
         public string applicationDescription { get; set; }
+        [Display(Name = "Status")]
         public Status status { get; set; }
+        [Display(Name = "Pending Functionalities")]
         public string functionalitiesToImplement { get; set; }
+        [Display(Name = "Expected UAT Delivery Date")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         public DateTime expectedUatDeliveryDate { get; set; }
+        [Display(Name = "Expected PROD Delivery Date")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         public DateTime prodDeploymentDate { get; set; }
+        [Display(Name = "Pending Info")]
         public string pendingInfo { get; set; }
+        [Display(Name = "Comments")]
         public string comments { get; set; }
     }
     public enum Status
